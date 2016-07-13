4fa0cf472ae0336e7cd7200fd0871b2f914af6a7
cchandurkar <cchandurkar@gmail.com>
2016-07-13 18:28:35 -0400

    export all series

---
+++ src/series/index.js
@@ -0,0 +1,10 @@
+export { default as sum } from './sum';
+export { default as mean } from './mean';
+export { default as median } from './median';
+export { default as mode } from './mode';
+export { default as range } from './range';
+export { default as sort } from './sort';
+export { default as isArithmetic } from './arithmetic';
+export { default as isGeometric } from './geometric';
+export { default as nthArithmetic } from './nthArithmetic';
+export { default as nthGeometric } from './nthGeometric';
