9148e58e8234034fe3d2f63d221e88bed173dd57
cchandurkar <cchandurkar@gmail.com>
2016-07-13 18:32:50 -0400

    exporting all from src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 export { default as isPrime } from './prime';
+export { default as isAbundant } from './abundant';
+export { default as isPerfect } from './perfect';
+export { default as isDeficient } from './deficient';
+export { default as isPerfectSquare } from './perfectSquare';
+export { default as divisors } from './divisors';
+
+// Import Series
+import * as series from './series';
+export { series as series };
